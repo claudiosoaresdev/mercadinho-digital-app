8367a09def398a7ed5e0a0d6f89729811c46695e
Claudio Soares <claudiolibanez@gmail.com>
2024-10-19 21:46:53 -0300

    first commit

---
+++ app/src/main/java/com/claudiosoaresdev/mercadinhodigital/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.claudiosoaresdev.mercadinhodigital
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.claudiosoaresdev.mercadinhodigital.ui.theme.MercadinhoDigitalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MercadinhoDigitalTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MercadinhoDigitalTheme {
+        Greeting("Android")
+    }
+}
